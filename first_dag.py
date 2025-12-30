d84eb1bf4504b97319632d757e9718f3077b875b
Rajesh Paruchuri <rajeshchowdary930@gmail.com>
2025-12-29 16:05:11 -0800

    Add dummy print statement for PR automation check
    Added a print statement for testing PR request automation.

--- first_dag.py
+++ first_dag.py
@@ -63,7 +63,7 @@ with DAG(
                     "volume": int(vals["5. volume"]),
                 }
             )
-
+        print("This is just a dummy line added to check the pr request automation in n8n")
         # sort ascending and keep last 90
         records.sort(key=lambda r: r["date"])
         if len(records) > 90:
